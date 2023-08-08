e8a8922ef25a5b58c7f193fb2e4c5a7217987e45
Z3r0 K3lv1n <84712952+z3r0-k3lv1n@users.noreply.github.com>
2023-08-09 09:33:45 +1000

    Update and rename shield.sh.rtf to shield.sh
    Refactor

---
+++ Sneakers/shield.sh
@@ -0,0 +1,13 @@
+#!/bin/bash\
+\
+# The Raise Shields Script\
+\
+# --------------------------------------------------------------------------------\
+#       This script aims to establish a vpn connection in order to protect internet\
+#       traffic.\
+#\
+# --------------------------------------------------------------------------------\
+\
+echo "Raising Shields."\
+echo ""\
+}
