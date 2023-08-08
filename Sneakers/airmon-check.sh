da812ee73246d7aacca4d762aeeab94a7a262b6a
Z3r0 K3lv1n <84712952+z3r0-k3lv1n@users.noreply.github.com>
2023-08-09 09:31:33 +1000

    Update and rename airmon-check.sh.rtf to airmon-check.sh
    Refactor

---
+++ Sneakers/airmon-check.sh
@@ -0,0 +1,10 @@
+#!/bin/bash\
+\
+# --------------------------------------------------------------------------------------------------\
+#       This script performs the following functions.\
+#                       1. Checks for processes that could interfere with aircrack.\
+# --------------------------------------------------------------------------------------------------\
+\
+\
+airmon-ng check wlan0\
+}
