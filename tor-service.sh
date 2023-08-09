0a8758363f437ac44bc5c0effd03017cbe3f99ab
Z3r0 K3lv1n <84712952+z3r0-k3lv1n@users.noreply.github.com>
2023-08-10 09:52:34 +1000

    Update tor_service.sh
    Refactor

--- tor-service.sh
+++ tor-service.sh
@@ -1,31 +1,31 @@
 #!/bin/bash\
-\
+
 # The Tor Service Script\
-\
-# --------------------------------------------------------------------------------\
-#       This script performs the following function.\
-#               1. Determine whether the Tor Service is active.\
-#               2. If the Tor Service is inactive it will switch the service on.\
-#               3. If the Tor Service is already on then it will do nothing.\
-# --------------------------------------------------------------------------------\
-\
-\
-str1="Active: active (exited)"\
-str2="Active: inactive (dead)"\
-tor.service\
-\
-clear\
-sleep 1\
-service tor status | grep Active\
-sleep 1\
-if [[ $TorActive != "Active: active" ]]\
-        then\
-        service tor start | grep Active\
-        sleep 1\
-        service tor status\
-	  sleep 10\
-    elif [[ condition ]]; then\
-        #statements\
-	echo \'93Your Tor service is switched on mate!\'94\
-fi\
+
+# --------------------------------------------------------------------------------
+#       This script performs the following function.
+#               1. Determine whether the Tor Service is active.
+#               2. If the Tor Service is inactive it will switch the service on.
+#               3. If the Tor Service is already on then it will do nothing.
+# --------------------------------------------------------------------------------
+
+
+str1="Active: active (exited)"
+str2="Active: inactive (dead)"
+tor.service
+
+clear
+sleep 1
+service tor status | grep Active
+sleep 1
+if [[ $TorActive != "Active: active" ]]
+        then
+        service tor start | grep Active
+        sleep 1
+        service tor status
+	  sleep 10
+    elif [[ condition ]]; then
+        #statements
+	echo \'93Your Tor service is switched on mate!
+fi
 }
