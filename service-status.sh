9d4114c519b3d347f667c669bfbaef69f3605352
Z3r0 K3lv1n <84712952+z3r0-k3lv1n@users.noreply.github.com>
2023-08-10 09:50:10 +1000

    Update service-status.sh
    Refactor

--- service-status.sh
+++ service-status.sh
@@ -1,21 +1,21 @@
-#!/bin/bash\
-\
-# --------------------------------------------------------------------------------------------------\
+#!/bin/bash
+
+# --------------------------------------------------------------------------------------------------
 #       This script performs the following functions.\
 #                       1. Switch Alfa WiFi Adapter to Monitor Mode.\
-#                       2. Shows the iwconfig setting and sorts for the line showing the current mode.\
-# --------------------------------------------------------------------------------------------------\
-\
-\
-echo ""\
-echo "The Wireless adapter is currently in the following mode:"\
-iwconfig wlan0 | grep Mode\
-echo ""\
-echo "The TOR Service is Currently:"\
-service tor status\
-echo ""\
-echo "The MAC Address is Currently Set to:"\
-macchanger wlan0 -s | grep "Current MAC"\
-echo ""\
-echo "The Permanent MAC Address of this Wireless Device is:"\
+#                       2. Shows the iwconfig setting and sorts for the line showing the current mode.
+# --------------------------------------------------------------------------------------------------
+
+
+echo ""
+echo "The Wireless adapter is currently in the following mode:"
+iwconfig wlan0 | grep Mode
+echo ""
+echo "The TOR Service is Currently:"
+service tor status
+echo ""
+echo "The MAC Address is Currently Set to:"
+macchanger wlan0 -s | grep "Current MAC"
+echo ""
+echo "The Permanent MAC Address of this Wireless Device is:"
 macchanger wlan0 -s | grep "Permanent MAC"}
